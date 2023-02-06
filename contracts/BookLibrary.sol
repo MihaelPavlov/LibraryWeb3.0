bb67233e1b35dc219d062d4050b42e0fd8eb2da7
MihaelPavlov <m.pavlov1405@abv.bg>
2023-02-06 20:36:57 +0200

    refactor book library

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -3,13 +3,6 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 import "./2_Owner.sol";
-import "./LibraryRegistry.sol";
-
-interface ILibraryRegsitry {
-    function register(string memory _username) external payable;
-
-    function readBook(address _contractAddress, string memory _bookName) external;
-}
 
 contract BookLibrary is Owner{
 
@@ -17,12 +10,6 @@ contract BookLibrary is Owner{
         string name;
         uint copies;
         address[] bookBorrowedAdresses;
-    }
-
-    ILibraryRegsitry public registry; // Can i check if this initialization is null. Is there a null?
-
-    function setRegistry(address _addr) public{
-        registry = ILibraryRegsitry(_addr);
     }
 
     bytes32[] public bookKeys;
@@ -45,11 +32,6 @@ contract BookLibrary is Owner{
         require(tempBookName.length > 0 && _copies > 0, "Book data is not valid");
         _;
     }
-
-    // modifier isRegisterdInLibrary(address _addr){
-    //     require(bytes(registry.isRegistered(_addr).username).length > 0,"You are not registered");
-    //     _;
-    // }
 
     // Add new book, if the book is already in the library we are adding only the copies
     function addBook(string memory _bookName, uint _copies) public isOwner isBookValid(_bookName, _copies){
@@ -89,25 +71,13 @@ contract BookLibrary is Owner{
         require(borrowedBook[msg.sender][bookName],"You don't have this book");
         
         borrowedBook[msg.sender][bookName] = false;
-        books[bookName].copies ++;
+        books[bookName].copies++;
 
         emit ReturnedBook(_bookName,msg.sender);
-
-        registry.readBook(address(this), _bookName);
-
-        // Delegetae call is sending the data from the caller 
-        // basically if we call readBook function with delegateCall the msg.sender will be the account address. Else if we use just call the msg.sender will be the contract address
-
-        // (bool isOkey,) = registry.delegatecall(abi.encodeWithSelector(LibraryRegistry.readBook.selector,address(this),bookName));
-        // require(isOkey,"Something failed");
     }
 
     // Helpers
     function getNumberOfBooks() public view returns (uint _numberOfBooks){
         return bookKeys.length;
-    }
-
-    function GetbookBytes(string memory _bookName) public view returns(uint   ){
-        return bytes(books[keccak256(abi.encodePacked(_bookName))].name).length;
     }
 }
